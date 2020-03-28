76adf339d803b91be409f9286478261e2a89c19c
Jean-Yves Girard <jeanyvesgirard@free.fr>
2020-03-28 14:58:29 +0100

    SCHNORR: multi sig ,passage  à n signature

--- schnorr/MultiSig/multisig.go
+++ schnorr/MultiSig/multisig.go
@@ -70,49 +70,49 @@ var GroupOrder = ED25519.NewBIGints(ED25519.CURVE_Order)
 
 // X est l aggregated key representant chacun des signers, il est noté ~X dans le papier https://eprint.iacr.org/2018/068.pdf
 // il peut être utilisé pour representer le compte
-func GetX_AggregatedPk(pk1 *ED25519.ECP,pk2 *ED25519.ECP )(*ED25519.ECP, *ED25519.BIG,*ED25519.BIG ) {
-
+func GetX_AggregatedPk(pk []*ED25519.ECP)(*ED25519.ECP, []*ED25519.BIG ) {
+    n:= len(pk)
    //Now we have X = summation of every participants H(L,P)P  - a=H(L,P)
    
+    a := make([]*ED25519.BIG,n)
+
     //L = H(set of each individual’s Public key P).
     Lhash:=sha256.New()
-    Lhash.Write([]byte(EcpToBytes(pk1)))
-    Lhash.Write([]byte(EcpToBytes(pk2)))
+    for i := 0 ; i< n ; i++ {
+          Lhash.Write([]byte(EcpToBytes(pk[i])))
+    }
     LBytes :=Lhash.Sum(nil) 
 
-    hash1 := sha256.New()
-    hash1.Write(LBytes)
-    hash1.Write([]byte(EcpToBytes(pk1)))
-    hash1Bytes :=hash1.Sum(nil) 
-
-    a1 := ED25519.FromBytes(hash1Bytes[:])
-    a1.Mod(GroupOrder)
-
-    X := pk1.Mul(a1)
-
-    hash2 := sha256.New()
-    hash2.Write(LBytes)
-    hash2.Write([]byte(EcpToBytes(pk2)))
-    hash2Bytes :=hash2.Sum(nil) 
-
-    a2 := ED25519.FromBytes(hash2Bytes[:])
-    a2.Mod(GroupOrder)
-    
-    X.Add(pk2.Mul(a2))
+    X := ED25519.NewECP()
+    for i := 0 ; i< n ; i++ {
+          hash1 := sha256.New()
+          hash1.Write(LBytes)
+          hash1.Write([]byte(EcpToBytes(pk[i])))
+          hash1Bytes :=hash1.Sum(nil) 
+          a[i] = ED25519.FromBytes(hash1Bytes[:])
+          a[i].Mod(GroupOrder)
+          if (i==0) {
+                X = pk[i].Mul(a[i])
+          } else {
+                X.Add(pk[i].Mul(a[i]))
+          }
+    }
 
    // fmt.Println("X: ",X.ToString())
 
-   return X,a1,a2
+   return X,a
 }
 
 
-func Sign_GetR_and_c(R1 *ED25519.ECP,R2 *ED25519.ECP,X *ED25519.ECP, z []byte)  (*ED25519.ECP, *ED25519.BIG ) {
+func Sign_GetR_and_c(R []*ED25519.ECP,X *ED25519.ECP, z []byte)  (*ED25519.ECP, *ED25519.BIG ) {
  
    //Each individual chooses their own unique r and calculates their own unique R = rG and R(sum) is the summation of all R.
     Rsum := ED25519.NewECP()
-    Rsum.Copy(R1)
-    Rsum.Add(R2)
-
+    Rsum.Copy(R[0])
+    
+    for i:=1; i< len(R); i++ {
+          Rsum.Add(R[i])
+    }
 
   //c also changes from BN equation. c = H(X, R(sum), z)*a. A new variable ‘a’ is introduced where each participant uses their unique public key to calculate a = H(L, P). So, c = H(X, R(sum), z)H(L, P).
 
@@ -144,14 +144,18 @@ func Sign_Get_s(HXRzBIG *ED25519.BIG, r *ED25519.BIG, a *ED25519.BIG, x *ED25519
 }
 
 
-func Sign_Get_S(s1 *ED25519.BIG,s2 *ED25519.BIG) (*ED25519.BIG){ 
-     return ED25519.Modadd(s1,s2,GroupOrder)
+func Sign_Get_S(s []*ED25519.BIG) (*ED25519.BIG){ 
+     Sum := ED25519.NewBIGcopy(s[0])
+     for i:=1;i<len(s);i++ {
+         Sum = ED25519.Modadd(Sum,s[i],GroupOrder)
+     }
+     return Sum
 }
 
 
-func Verify_MSIG(pk1 *ED25519.ECP, pk2 *ED25519.ECP, RSum *ED25519.ECP, SSum *ED25519.BIG, msg []byte) bool {
+func Verify_MSIG(pk []*ED25519.ECP, RSum *ED25519.ECP, SSum *ED25519.BIG, msg []byte) bool {
 
-   X,_,_ := GetX_AggregatedPk(pk1,pk2)
+   X,_ := GetX_AggregatedPk(pk)
 
    hash := sha256.New()
    hash.Write(EcpToBytes(X))
@@ -180,51 +184,62 @@ func main() {
    rnd := GetRand()
    msg := "Hello"
 
-   sk1 := RandModOrder(rnd)
-   pk1 := GenG.Mul(sk1)
-   fmt.Println("pk1: ",pk1.ToString())
+   n:= 20
+   var i int
 
-   sk2 := RandModOrder(rnd)
-   pk2 := GenG.Mul(sk2)
-   fmt.Println("pk2: ",pk2.ToString())
+   sk := make([]*ED25519.BIG,n)
+   pk := make([]*ED25519.ECP,n)
+
+   for i=0;i<n;i++ {
+      sk[i] = ED25519.NewBIG()
+      sk[i] = RandModOrder(rnd)
+      pk[i] = ED25519.NewECP()
+      pk[i] = GenG.Mul(sk[i])
+      fmt.Println("pk[",i,"]",pk[i].ToString())
+   } 
 
 
    fmt.Println()
 
-  // Generation des r pour chacun
-   r1 := RandModOrder(rnd)
-   r2 := RandModOrder(rnd)
+   r := make([]*ED25519.BIG,n)
+   R := make([]*ED25519.ECP,n)
 
-   // envoi des R aux autres signatires avec ti=H(Ri) pour verification à reception
-   R1 := GenG.Mul(r1)
-   R2 := GenG.Mul(r2)
+   for i=0;i<n;i++ {
+      r[i] = ED25519.NewBIG()
+      r[i] = RandModOrder(rnd)
+      R[i] = ED25519.NewECP()
+      R[i] = GenG.Mul(r[i])
+   } 
 
+   a := make([]*ED25519.BIG,n)
 
-   X,a1,a2 := GetX_AggregatedPk(pk1,pk2)
+   X,a := GetX_AggregatedPk(pk)
 
    fmt.Println("addresse X aggrégée: ",X.ToString())
 
    fmt.Println("\n")
 
    // envoi des Ri aux autres signataires
-   RSum, c := Sign_GetR_and_c(R1,R2, X, []byte(msg))
+   RSum, c := Sign_GetR_and_c(R, X, []byte(msg))
 
-   s1 := Sign_Get_s(c, r1, a1, sk1)
-   s2 := Sign_Get_s(c, r2, a2, sk2)
+
+   s := make([]*ED25519.BIG,n)
+   for i =0 ; i< n ; i++ {
+      s[i] = Sign_Get_s(c, r[i], a[i], sk[i])
+   }
+
 
    // envoi des si aux autres signataures
-   SSum :=  Sign_Get_S(s1,s2)
-
+   SSum :=  Sign_Get_S(s)
 
    fmt.Println("Multi Sig pour le message",msg)
    fmt.Println("Rsum: ", RSum.ToString())
    fmt.Println("Ssum: ", SSum.ToString())
 
-
    // Verification pour un membre
    // à partir des clés, on calcul les ai et la clé aggrege X
    fmt.Println("\n\nVérification par un des membres, il doit y avoir égalité")
 
-   Verify_MSIG(pk1 , pk2 , RSum , SSum ,[]byte(msg))
+   Verify_MSIG(pk , RSum , SSum ,[]byte(msg))
 
 }
