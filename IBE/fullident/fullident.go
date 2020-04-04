1a7ee0e2e382363ed8ce73d422e4bf706254d39e
Jean-Yves Girard <jeanyvesgirard@free.fr>
2020-04-04 19:02:22 +0200

    [fullident] encryption

---
+++ IBE/fullident/fullident.go
@@ -0,0 +1,164 @@
+package main
+
+//Identity-based encryption [Boneh and Franklin 2001]
+
+
+import (
+	"fmt"
+	b64 "encoding/base64"
+	//"os"
+//	"strconv"
+	"crypto/rand"
+	amcl "github.com/miracl/core/go/core"
+	curve "github.com/miracl/core/go/core/BLS48581"
+	"golang.org/x/crypto/sha3"
+)
+
+var GenG1 = curve.ECP_generator()
+var GenG2 = curve.ECP8_generator()
+
+// GenGT is a generator of Group GT
+var GenGT = curve.Fexp(curve.Ate(GenG2, GenG1))
+
+
+
+// GetRand returns a new *amcl.RAND with a fresh seed
+func GetRand() (*amcl.RAND) {
+	seedLength := 32
+	b := make([]byte, seedLength)
+	_, err := rand.Read(b)
+	if err != nil {
+		 fmt.Print(err ,"error getting randomness for seed")
+		 return nil
+	}
+	rng := amcl.NewRAND()
+	rng.Clean()
+	rng.Seed(seedLength, b)
+	return rng
+}
+
+func RandModOrder(rng *amcl.RAND) *curve.BIG {
+	// curve order q
+	q := curve.NewBIGints(curve.CURVE_Order)
+
+	// Take random element in Zq
+	return curve.Randomnum(q, rng)
+}
+
+
+
+// nh longueur du hash à generer qui depend ici de la longueur du message. 
+// hash de Gt vers {0,1}^m
+
+func H2(n *curve.FP48 , nh int) ([]byte) {
+    // FP48 a 3 FP16 qui a 2 FP8 qui 2 FP4 qui 2 FP2 qui 2 FP
+	nn := make([]byte,int(3*2*2*2*2*(curve.MODBYTES)))    
+	n.ToBytes(nn)
+
+	h := make([]byte,nh)
+
+	hash:=sha3.NewShake256()
+	hash.Write(nn)
+	hash.Read(h)
+	return h
+}
+
+func xor( s1 []byte, s2 []byte) ([]byte) {
+	s := make([]byte,len(s1))
+	for i := 0 ; i < len(s1); i++ {
+		s[i] = s1[i]^s2[i]
+	}
+	return s
+}
+
+
+func H1( a []byte ,b []byte  ) (*curve.BIG) {	
+	h := make([]byte,curve.MODBYTES)
+
+	hash:=sha3.NewShake256()
+	hash.Write(a)
+	hash.Write(b)
+	hash.Read(h)
+	return curve.FromBytes(h)
+}
+
+
+
+func G1( a []byte , nh int ) ([]byte) {	
+	h := make([]byte,nh)
+
+	hash:=sha3.NewShake256()
+	hash.Write(a)
+	hash.Read(h)
+	return h
+}
+
+
+
+
+func main() {
+   msg := "Bonjour comment allez vous"
+
+	//initialization de la master key pour la central authority
+   rnd := GetRand()
+   s := RandModOrder(rnd)
+   Ppub := GenG2.Mul(s)
+
+   //encryption
+   //key generation, envoi d un fichier à jeanyves.girard@fr.ibm.com
+   id := "jeanyves.girard@fr.ibm.com"
+   Qid:=curve.ECP_mapit([]byte(id))
+
+   //generation de n charactere aléatoire
+   sigma :=  make([]byte, len(msg))
+    _, _ = rand.Read(sigma)
+
+    r := curve.NewBIG()
+    r = H1(sigma,[]byte(msg))
+
+    CU := GenG2.Mul(r)
+
+    Gid := curve.Fexp(curve.Ate(Ppub,Qid))
+    CV := xor(sigma,H2(Gid.Pow(r),len(msg)))
+
+    CW := xor([]byte(msg),G1(sigma,len(msg))) 
+
+    fmt.Println(CU.ToString())
+	fmt.Println(b64.StdEncoding.EncodeToString(CV))
+	fmt.Println(b64.StdEncoding.EncodeToString(CW))
+
+
+
+	//decryption en utilisation C <U,V,W>
+
+/*
+   Sa := Pa.Mul(s)
+
+   // Encryption avec la clé publique
+   l := RandModOrder(rnd)
+   R := GenG2.Mul(l)
+   h2 := H2(curve.Fexp(curve.Ate(Ppub,Pa)).Pow(l),len(msg))
+
+   c:= xor(h2,[]byte(msg))
+
+   fmt.Println("R=> ", R. ToString())
+   fmt.Println("c=> ",b64.StdEncoding.EncodeToString(xor(c,[]byte(msg))))
+
+   c_b64 := b64.StdEncoding.EncodeToString(xor(h2,[]byte(msg)))
+   // ce serait de le padding pour eviter de savoir le nombre de lettre ds le message
+
+
+
+   //decryption avec ma clé privé
+
+   c_decoded , _ := b64.StdEncoding.DecodeString(c_b64)
+
+   tt := curve.Fexp(curve.Ate(R,Sa))
+   //fmt.Println(tt.ToString())
+   H2_d := H2(tt,len(msg))
+   m_d := xor([]byte(c_decoded), H2_d)
+
+   //message décodé
+   fmt.Print(string(m_d))
+*/
+}
