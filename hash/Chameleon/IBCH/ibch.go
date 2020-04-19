53b2418248718a28760a809f3f72eb72f4f47387
Jean-Yves Girard <jeanyvesgirard@free.fr>
2020-04-19 08:57:30 +0200

    [IBCH] fonctionnel - début refactoring

--- hash/Chameleon/IBCH/ibch.go
+++ hash/Chameleon/IBCH/ibch.go
@@ -10,30 +10,20 @@ import (
 )
 
 
-//Lin an Al. triple trapdoor (extend double trapdoor function)
-func ECDLChameleonHash(K *curve.ECP  , Y *curve.ECP,  m []byte, r *curve.BIG) (*curve.ECP) {
-	ftk := f_tk(H1(m),K)
 
-	res := K.Mul2(ftk,Y,r)
-	return res
 
-}
+func  f(a []*(curve.BIG) ,x *curve.BIG ) (*curve.BIG){
 
-func f_tk (a *curve.BIG,b *curve.ECP ) (*curve.BIG){
-	nn := make([]byte,int(8*(curve.MODBYTES)))    
+	tot := curve.NewBIGcopy(a[0])
+	X := curve.NewBIGcopy(x)
 
-	n := make([]byte,int(8*(curve.MODBYTES))) 
-	a.ToBytes(n)
-
-	hash:=sha3.NewShake256()
-	hash.Write(n)
-	hash.Write(util.EcpToBytes(b))
-	hash.Read(nn)
-
-	d := curve.FromBytes(nn)
-	d.Mod(util.GroupOrder)
-
-	return d
+	if len(a) >0 {
+		for i:=1;  i<len(a);i++ {
+			tot = curve.Modadd(tot,curve.Modmul(a[i],X,util.GroupOrder),util.GroupOrder)
+			X = curve.Modmul(x,X,util.GroupOrder)
+		}	
+	}
+	return tot
 }
 
 func H1( a []byte ) (*curve.BIG) {	
@@ -47,46 +37,92 @@ func H1( a []byte ) (*curve.BIG) {
 	return d
 }
 
+func CHash(pk []*(curve.ECP8), ID []byte,L []byte,m []byte,R *curve.ECP) (*curve.FP48) {
+	IDbar:=make([]byte,len(ID)+len(L))
+	copy(IDbar,ID)
+	IDbar=append(IDbar,L...)
 
-func getCollision(K *curve.ECP, x *curve.BIG , k *curve.BIG, mPrime []byte, m []byte, r *curve.BIG) (*curve.BIG) {
-	f1 := f_tk(H1(m),K)
-	f2 := f_tk(H1(mPrime),K)
-	f := util.Modsub(f1,f2,util.GroupOrder)
-	invx := curve.NewBIGcopy(x)
-	invx.Invmodp(util.GroupOrder)
-	rPrime := curve.Modmul(k,invx,util.GroupOrder)
-	rPrime = curve.Modmul(rPrime,f,util.GroupOrder)
-	return curve.Modadd(rPrime,r,util.GroupOrder)
+	hash := curve.Fexp(curve.Ate(util.GenG2,R))
+	tmpp := curve.Fexp(curve.Ate(fid(pk,H1(ID)),curve.ECP_map2point(H1(IDbar)).Mul(H1(m))))
+	hash.Mul(tmpp)
+
+	return hash
 }
 
+func Forge(skid *curve.BIG, ID []byte,L []byte,mPrime []byte,m []byte, R *curve.ECP) (*curve.ECP) {
+	IDbar:=make([]byte,len(ID)+len(L))
+	copy(IDbar,ID)
+	IDbar=append(IDbar,L...)
+
+	H := curve.ECP_map2point(H1(IDbar))
+	diff := util.Modsub(H1(m),H1(mPrime),util.GroupOrder)
+	mul := curve.Modmul(skid,diff,util.GroupOrder)
+	Htemp := H.Mul(mul)
+
+	Htemp.Add(R)
+	return Htemp
+}
+
+func  fid(pk []*(curve.ECP8) ,x *curve.BIG ) (*curve.ECP8){
+	tot := curve.NewECP8()
+	tot.Copy(pk[0])
+	X := curve.NewBIGcopy(x)
+	//var m int
+	if len(pk) >0 {
+		for i:=1;  i<len(pk);i++ {
+			tot.Add(pk[i].Mul(X))   // retourne un entier
+			X = curve.Modmul(x,X,util.GroupOrder)
+		}	
+	}
+	return tot
+}
+
+
+
 func main() {
-
 	rnd := util.GetRand()
-	//Recipient Trapdoor private key
-	k := util.RandModOrder(rnd)
-	x := util.RandModOrder(rnd)
-	//tk := util.RandModOrder(rnd)
 
-	//Recipient public hash keys
-	K := util.GenG1.Mul(k)
-	Y := util.GenG1.Mul(x)
 
+	// Travail chez le receveur
+	// Pour une identité, il établit une clé publique qu'il va fournir pk
+	// Il garde une clé privé pour pouvoir forger un nouveau message skid
+	t := 5
+	a := make([]*curve.BIG,t)
+	pk := make([]*curve.ECP8,t)
+
+	for i:=0;i<t;i++ {
+		a[i] = util.RandModOrder(rnd)
+		pk[i] = util.GenG2.Mul(a[i])
+	}
+
+	ID := []byte("jeanyves.girard@fr.ibm.com")
+	skid := f(a,H1(ID))
+
+	//Master Key
+//	x := util.RandModOrder(rnd)
+//	sk := f(a,x)
+
+	
+
+//fmt.Println(util.GenG2.Mul(f(a,H1(ID))).ToString())
+//fmt.Println(fid(pk,H1(ID)).ToString())
+
+	L := []byte("robert@royalcanin.fr")
+	m := []byte("salut les copains")
+
+	mPrime := []byte("BigBisous")
 
 
 	r := util.RandModOrder(rnd)
-	m := []byte("Hello salut la compagnie")
-
-	fmt.Println("Hash(",string(m),",",r.ToString())
-	fmt.Println(ECDLChameleonHash(K,Y,m,r).ToString())
-
-	mPrime := []byte("blablabla je vais tout masquer")
-
-	//Hm := util.H1(m)
-	rPrime := getCollision(K,x,k,mPrime,m,r)
+	R := util.GenG1.Mul(r)
 
 
-	fmt.Println()
-	fmt.Println("Hash(",string(mPrime),",",rPrime.ToString())
-	fmt.Println(ECDLChameleonHash(K,Y,mPrime,rPrime).ToString())
+	S1:= CHash(pk,ID,L,m,R)
+
+	RPrime := Forge(skid,ID,L,mPrime,m,R)
+
+	S2 := CHash(pk,ID,L,mPrime,RPrime)
+
+	fmt.Println(S1.Equals(S2))
 
 }
